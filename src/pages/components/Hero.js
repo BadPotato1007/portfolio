1bb876babdef10601e4699e5df1a7872926c532c
neerav gupta <neeravgupta07@gmail.com>
2022-10-31 16:38:33 +0530

    modified:   src/pages/components/Hero.js

--- src/pages/components/Hero.js
+++ src/pages/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './components.css'
+import ReactTypingEffect from 'react-typing-effect';
 
 const Hero = () => {
     return (
@@ -17,9 +18,10 @@ const Hero = () => {
                     <td><p class="bigname">   &nbsp;&nbsp;  eerav, </p></td>
                 </tr>
             </table><br/>
-            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium  rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                <ReactTypingEffect typingDelay="500" eraseDelay="1200" text={["Web developer.", "Robotics engineer.", "Student.", "Artist.", "Pianist."]} className="bigname"/><br/>
+            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium  rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800" >
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-transparent">
-                Green to blue
+                Contact Me
             </span>
             </button>
             
