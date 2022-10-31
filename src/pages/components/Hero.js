232c2a79421d0cf6fbe29c5faa3c9b7a5fb4a52e
neerav gupta <neeravgupta07@gmail.com>
2022-10-31 16:29:34 +0530

    modified:   yarn.lock

--- src/pages/components/Hero.js
+++ src/pages/components/Hero.js
@@ -17,6 +17,11 @@ const Hero = () => {
                     <td><p class="bigname">   &nbsp;&nbsp;  eerav, </p></td>
                 </tr>
             </table><br/>
+            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium  rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-transparent">
+                Green to blue
+            </span>
+            </button>
             
             
             
--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './components.css'
 const Navbar = () => {
   return (
-    <div>
+    <div className='nav-bar'>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <h1 class="hover-underline-animation text-2xl pt-6">About</h1>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
