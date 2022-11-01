863aabbb436d4e24565bb75a93f8b80f2b267904
neerav gupta <neeravgupta07@gmail.com>
2022-11-01 18:41:22 +0530

    modified:   src/pages/components/About.js

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -6,8 +6,20 @@ const About = () => {
 
             About Me
             <br/>
+            I am a 15 year old web developer, with keen intrest in robotics and music. 
+            I started coding 5 years ago and know over 8 programming languages. I have 
+            been said to have good leadership skills, and excelent organisation. I love
+            to listen to music in my spare time and aspire to build a muilti billion
+            dollar company.
         </div>
     );
 };
 
 export default About;
+
+/*
+I am a 15 year old web developer, with keen intrest in robotics and music. 
+I started coding 5 years ago and know over 8 programming languages. I have 
+been said to have good leadership skills, and excelent organisation. I love
+to listen to music in my spare time and aspire to build a muilti billion
+dollar company. */
